22e738b50e9f8ab4719852656aab0fd027a009f4
Harshit Sharma <harshitsharma967@gmail.com>
2024-07-07 21:29:57 -0400

    made changes to fix warnings

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,17 +5,17 @@ import './Social.css'; // Ensure you import your CSS file
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="mailto:harshitsharma967@gmail.com" className="home__social-icon email" target="_blank">
+            <a href="mailto:harshitsharma967@gmail.com" className="home__social-icon email" target="_blank" rel="noreferrer">
                 <FiMail />
             </a>
-            <a href="https://github.com/Posxidon" className="home__social-icon github" target="_blank">
+            <a href="https://github.com/Posxidon" className="home__social-icon github" target="_blank" rel="noreferrer">
                 <FiGithub />
             </a>
-            <a href="https://www.linkedin.com/in/harshit-rakesh-sharma/" className="home__social-icon linkedin" target="_blank">
+            <a href="https://www.linkedin.com/in/harshit-rakesh-sharma/" className="home__social-icon linkedin" target="_blank" rel="noreferrer">
                 <FiLinkedin />
             </a>
-        </div> 
-    ); 
+        </div>
+    );
 }
 
 export default Social;
