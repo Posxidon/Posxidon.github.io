98918ea97d8eee81c9e8662867e843949442fbe8
Harshit Sharma <harshitsharma967@gmail.com>
2024-07-08 21:57:40 -0400

    updated project

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -4,11 +4,22 @@ import StockMarket from "../../assets/StockMarket.png";
 import VoiceAssistant from "../../assets/project3.jpeg";
 import Socket from "../../assets/socket.jpeg";
 import GroupWatch from "../../assets/groupwatch.jpeg";
+import Portfolio from "../../assets/portfolio1.PNG";
 
 export const projectsData = [
     
     {
         id: 1,
+        image: Portfolio,
+        title: "Harshit's Portfolio",
+        techStack: ["React", "CSS", "HTML", "EmailJS"],
+        url: "https://posxidon.github.io/"
+    },
+
+
+
+    {
+        id: 2,
         image: GroupWatch,
         title: "GroupWatch",
         techStack: ["TypeScript", "Tailwind CSS", "PostCSS", "Node.js"],
@@ -17,7 +28,7 @@ export const projectsData = [
 
 
     {
-        id: 2,
+        id: 3,
         image: Socket,
         title: "Client-Server File Transfer with Socket API",
         techStack: ["Python", "Socket", "sys", "os"],
@@ -25,7 +36,7 @@ export const projectsData = [
     },
     
     {
-        id: 3,
+        id: 4,
         image: WeatherApp,
         title: "WeatherApp",
         techStack: ["Python", "Django", "OpenWeatherMap API"],
@@ -33,21 +44,21 @@ export const projectsData = [
     },
     
     {
-        id: 4,
+        id: 5,
         image: Pneumonia,
         title: "Pneumonia Detection using ML",
         techStack: ["Python", "TensorFlow", "NumPy", "Matplotlib","pathlib"],
         url: "https://github.com/Posxidon/Pneumonia-Detection-using-ML"
     },
     {
-        id: 5,
+        id: 6,
         image: StockMarket,
         title: "Stock Market Predictor",
         techStack: ["Python", "yfinance", "pandas","scikit-learn","precision_score","sklearn.metrics","matplotlib","plot"],
         url: "https://github.com/Posxidon/stockmarketpredictor"
     },
     {
-        id: 6,
+        id: 7,
         image: VoiceAssistant,
         title: "Voice Assistant",
         techStack: ["Python", "Speech Recognition", "Speech Synthesis", "pywhatkit","pyjokes"],
